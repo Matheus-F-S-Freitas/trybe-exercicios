efc6a3a7b5cd3edf79ef6ff7e59dbe9d1201d1fd
Matheus Freitas <matheus2002fernando@gmail.com>
2023-02-27 16:39:10 -0300

    Iniciando novo módulo

---
+++ fundamentos/secao-3-introducao-a-JS/dia-1-primeiros-passos/para_fixar-1.js
@@ -0,0 +1,6 @@
+let myName = "Matheus"
+let birthCity = "São José dos Campos";
+let birthYear = 2002;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
---
+++ fundamentos/secao-3-introducao-a-JS/dia-1-primeiros-passos/para_fixar-2.js
@@ -0,0 +1,5 @@
+let base = 5;
+let height = 8;
+let area = base * height;
+let perimeter = (5 * 2) + (8 * 2);
+console.log(perimeter, area)
