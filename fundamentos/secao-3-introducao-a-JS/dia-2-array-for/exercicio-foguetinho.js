f0ef2c340e89216da00cd9082532d0e24181185f
Matheus Freitas <matheus2002fernando@gmail.com>
2023-02-28 20:27:56 -0300

    Consegui o requisito graças a colega Corina

--- fundamentos/secao-3-introducao-a-JS/dia-2-array-for/exercicio-foguetinho.js
+++ fundamentos/secao-3-introducao-a-JS/dia-2-array-for/exercicio-foguetinho.js
@@ -6,9 +6,9 @@ V Some todos os valores contidos no array e imprima o resultado.
 V Calcule e imprima a média aritmética dos valores contidos no array.
      A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
 
-X Com base no código que acabou de gerar, faça com que: caso o valor final seja maior que 20, imprima a mensagem “Valor maior que 20”; e, caso não seja maior que 20, imprima a mensagem “Valor menor ou igual a 20”.
+V Com base no código que acabou de gerar, faça com que: caso o valor final seja maior que 20, imprima a mensagem “Valor maior que 20”; e, caso não seja maior que 20, imprima a mensagem “Valor menor ou igual a 20”.
 
-X Utilizando for, descubra o maior valor contido no array e imprima-o.
+V Utilizando for, descubra o maior valor contido no array e imprima-o.
 
 X Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “Nenhum valor ímpar encontrado”.
 
@@ -24,14 +24,10 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let some = 0;
 
-for (let index = 0; index < numbers.length; index += 1){
-    some += numbers[index]
+for (let index = 0; index < numbers.length; index += 1){//percorrerá numbers
+    if(numbers[index] > some){
+        some = numbers[index]
+    }
     
 }
-let media = some / numbers.length;
-if(media > 20){
-    console.log("Valor maior que 20")
-}
-else if(media <= 20){
-    console.log("Valor menor ou igual a 20")
-}
+console.log(some)
