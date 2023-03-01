da9eab654122d0dc0f86a0bd59729129f65c13b1
Matheus Freitas <matheus2002fernando@gmail.com>
2023-03-01 16:20:22 -0300

    fazendo o ultimo praticar um pouco antes da aula ao vivo

--- fundamentos/secao-3-introducao-a-JS/dia-3-functions/praticar-um-pouco-3.js
+++ fundamentos/secao-3-introducao-a-JS/dia-3-functions/praticar-um-pouco-3.js
@@ -1,9 +1,22 @@
-// Agora iremos escrever um programa que irá excluir um(a) cliente já existente do array de clientes do TrybeBank. Certifique-se de que a função deve receber um parâmetro do tipo string e imprimir uma mensagem de erro, caso o parâmetro não seja do tipo string e caso o(a) cliente não exista dentro do array.
+// Refatore a função removeCliente para diminuir sua complexidade e quebrá-la em funções menores.
 
 let clientesTrybeBank = ['Ada', 'John', 'Gus'];
 
-function remove(client){
-    clientesTrybeBank.splice(client, )
-}
-remove("John")
-console.log(clientesTrybeBank)
+function removeCliente(cliente) {
+  if (typeof cliente === 'string') {
+    let clienteEncontrado = false;
+    for (let index = 0; index < clientesTrybeBank.length; index += 1) {
+      if (cliente === clientesTrybeBank[index]) {
+        clientesTrybeBank.splice(index, 1);
+        clienteEncontrado = true;
+        return 'Cliente excluída(o) com sucesso.';
+      }
+    }
+
+    if (clienteEncontrado === false) {
+      return 'Cliente não encontrado'
+    }
+  } else {
+    return 'O parâmetro passado deve ser do tipo "string"!';
+  }
+};
