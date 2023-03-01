655764ac4c46c8610801fd9e8facb0be1f041038
Matheus Freitas <matheus2002fernando@gmail.com>
2023-03-01 14:51:43 -0300

    Finalizado com uma ajudinha do gabarito(dessa vez foi bem pequena mesmo)

--- fundamentos/secao-3-introducao-a-JS/dia-3-functions/praticar-um-pouco-2.js
+++ fundamentos/secao-3-introducao-a-JS/dia-3-functions/praticar-um-pouco-2.js
@@ -1,5 +1,14 @@
 // Faça um programa para adicionar clientes ao array de clientes do TrybeBank. Certifique-se de que a função deve receber um parâmetro do tipo string e imprimir uma mensagem de erro caso o parâmetro não seja do tipo string.
-// Copiar
+
 let clientesTrybeBank = ['Ada', 'John', 'Gus'];
 
-// escreva aqui sua função
+function newClients(name){
+    if(typeof name !== "string"){
+        return "Erro, insira um nome"
+    }
+    else {
+        clientesTrybeBank.push(name)
+    }
+}
+newClients('julia')
+console.log(clientesTrybeBank)
