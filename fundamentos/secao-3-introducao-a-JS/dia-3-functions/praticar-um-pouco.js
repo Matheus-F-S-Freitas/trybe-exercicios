9b7a9a5985bc24e55a4bc221660c7af17e30f196
Matheus Freitas <matheus2002fernando@gmail.com>
2023-03-01 14:16:12 -0300

    Dividindo o saldo e finalizando

--- fundamentos/secao-3-introducao-a-JS/dia-3-functions/praticar-um-pouco.js
+++ fundamentos/secao-3-introducao-a-JS/dia-3-functions/praticar-um-pouco.js
@@ -1,5 +1,14 @@
+/*Considere a variável saldo, que representa a quantia em conta de determinada(o) cliente do TrybeBank e escreva quatro programas que:
+
+Adicione um valor ao saldo.
+Subtraia um valor do saldo.
+Multiplique o valor do saldo por uma taxa.
+Divida o valor do saldo. */
+
+
+
 function bank(saldo){
-    saldo = (saldo + 250 - 100) * (saldo * 0.01)
+    saldo = (saldo + 250 - 100) * (saldo * 0.01) / 2
     return saldo
 }
 
