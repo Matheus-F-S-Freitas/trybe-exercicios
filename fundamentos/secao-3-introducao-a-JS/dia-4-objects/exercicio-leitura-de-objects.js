d6a00ead2efb268f2cdc454319b823a081d15b40
Matheus Freitas <matheus2002fernando@gmail.com>
2023-03-02 20:55:59 -0300

    Começando segunda parte do primeiro exercicio

---
+++ fundamentos/secao-3-introducao-a-JS/dia-4-objects/exercicio-leitura-de-objects.js
@@ -0,0 +1,19 @@
+// Acesse as chaves nome, sobrenome e titulo – esta encontra-se dentro da chave livrosFavoritos – e faça um console.log no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
+
+// Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a essa chave um objeto que contenha as seguintes informações:
+
+
+
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+        {
+            titulo: 'O Pior Dia de Todos',
+            autor: 'Daniela Kopsch',
+            editora: 'Tordesilhas',
+        },
+    ],
+};
