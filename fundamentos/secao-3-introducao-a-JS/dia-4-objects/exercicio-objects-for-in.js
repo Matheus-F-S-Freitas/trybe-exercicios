4801a8a7775024a9f55291e1d7a11072d11287a9
Matheus Freitas <matheus2002fernando@gmail.com>
2023-03-02 20:31:27 -0300

    Finalizado primeiro exercicio

---
+++ fundamentos/secao-3-introducao-a-JS/dia-4-objects/exercicio-objects-for-in.js
@@ -0,0 +1,12 @@
+//Com base neste objeto, faça os exercícios a seguir.
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+console.log(`Bem-vinda, ${info.personagem}`)
+
+//Imprima no console uma mensagem de boas-vindas para a personagem acima, na qual tenha o nome dela. Para isso, utilize a sintaxe meuObjeto.chave. Então, o valor esperado no console é:
+
+//Bem-vinda, Margarida
