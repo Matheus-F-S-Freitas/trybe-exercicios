e8ae6b5f19f952fcb2053928663ae868c438d08e
Matheus Freitas <matheus2002fernando@gmail.com>
2023-03-02 14:53:49 -0300

    Começando dia 4, refatorando código

---
+++ fundamentos/secao-3-introducao-a-JS/dia-4-objects/para-fixar-1.js
@@ -0,0 +1,10 @@
+//Crie uma variável player e atribua a ela um objeto que contenha as variáveis listadas na sequência.
+let player = {
+    name: "Marta",
+    lastName: "Silva",
+    age: 34,
+    medals: {
+        golden: 2,
+        silver: 3
+    },
+};
