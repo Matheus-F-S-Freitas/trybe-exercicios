8b7004810760c83b2dbd7c684eded0d7f7cb617b
Matheus Freitas <matheus2002fernando@gmail.com>
2023-03-02 20:36:04 -0300

    Finalizado segunda parte*correção: não terminei tudo rsrs*

--- fundamentos/secao-3-introducao-a-JS/dia-4-objects/exercicio-objects-for-in.js
+++ fundamentos/secao-3-introducao-a-JS/dia-4-objects/exercicio-objects-for-in.js
@@ -1,12 +1,23 @@
 //Com base neste objeto, faça os exercícios a seguir.
 
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
-console.log(`Bem-vinda, ${info.personagem}`)
 
 //Imprima no console uma mensagem de boas-vindas para a personagem acima, na qual tenha o nome dela. Para isso, utilize a sintaxe meuObjeto.chave. Então, o valor esperado no console é:
 
 //Bem-vinda, Margarida
+//_Finalizado_
+
+//Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. Para isso, use a sintaxe meuObjeto['chave'] = valor. Então, o valor esperado no console é:
+
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+info["recorrente"] = "sim"
+for (let index in info){
+    console.log(info[index])
+}
+console.log(`Bem-vinda, ${info.personagem}`)
+
+
