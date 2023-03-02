c74f474285145bb7b5b19b01e3d7f515e77f8a3b
Matheus Freitas <matheus2002fernando@gmail.com>
2023-03-02 15:04:08 -0300

    Finalizado com sucesso

--- fundamentos/secao-3-introducao-a-JS/dia-4-objects/para-fixar-1.js
+++ fundamentos/secao-3-introducao-a-JS/dia-4-objects/para-fixar-1.js
@@ -11,6 +11,9 @@ let player = {
 
 // Acesse as chaves name, lastName e age por meio da sintaxe meuObjeto.chave e concatene as informações para imprimir no console uma mensagem no seguinte formato: “A jogadora Marta Silva tem 34 anos de idade”.
 
-console.log(`A jogadora ${player.name} ${player.lastName} tem ${player.age} anos de idade`)
+player["bestInTheWorld"] = [1900, 1980, 2000, 2010, 2011, 2300];
+console.log(`A jogadora ${player.name} ${player.lastName} tem ${player.age} anos de idade
+Foi a melhor do mundo em ${player["bestInTheWorld"]}`)
 
 // Adicione ao objeto a chave bestInTheWorld por meio da sintaxe meuObjeto.chave = valor e atribua a essa chave um array que contenha as datas em que a jogadora Marta foi considerada a melhor do mundo.
+
