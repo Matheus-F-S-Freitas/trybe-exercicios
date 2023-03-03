662b86afd220d5ba436f9a453f475f67bdae346d
Matheus Freitas <matheus2002fernando@gmail.com>
2023-03-02 21:34:59 -0300

    Exercicio 1 finalizado com sucesso e com zero ajuda do gabarito (literalmente kkkkkk)

--- fundamentos/secao-3-introducao-a-JS/dia-4-objects/exercicio-leitura-de-objects.js
+++ fundamentos/secao-3-introducao-a-JS/dia-4-objects/exercicio-leitura-de-objects.js
@@ -1,19 +1,28 @@
 // Acesse as chaves nome, sobrenome e titulo – esta encontra-se dentro da chave livrosFavoritos – e faça um console.log no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
 
-// Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a essa chave um objeto que contenha as seguintes informações:
-
-
-
 
 let leitor = {
     nome: 'Julia',
     sobrenome: 'Pessoa',
     idade: 21,
-    livrosFavoritos: [
+    livrosFavoritos:[
         {
             titulo: 'O Pior Dia de Todos',
             autor: 'Daniela Kopsch',
             editora: 'Tordesilhas',
         },
+        {
+            titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+            autor: 'JK Rowling',
+            editora: 'Rocco',
+        },
     ],
 };
+
+// console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama '${leitor.livrosFavoritos[0].titulo}'`)
+console.log(`Julia tem ${leitor.livrosFavoritos.length} livros favoritos`)
+
+// Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a essa chave um objeto que contenha as seguintes informações:
+
+
+
