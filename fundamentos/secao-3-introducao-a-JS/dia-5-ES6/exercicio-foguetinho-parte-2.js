7649f49cb87419e2a63b93bd950c165e23d3b8f1
Matheus Freitas <matheus2002fernando@gmail.com>
2023-03-03 19:36:39 -0300

    Iniciando parte 2 do exercicio do foguetinho

---
+++ fundamentos/secao-3-introducao-a-JS/dia-5-ES6/exercicio-foguetinho-parte-2.js
@@ -0,0 +1,2 @@
+// Agora, você verá algumas especificações de algoritmos para desenvolver. É fundamental que você utilize o que estudou de let, const, arrow functions, template literals e ternary operator.
+
