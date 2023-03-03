07f7bcc63f0fed0df05180f80b86a2b0220e9c8c
Matheus Freitas <matheus2002fernando@gmail.com>
2023-03-03 19:05:22 -0300

    Concluido 1.2

--- fundamentos/secao-3-introducao-a-JS/dia-5-ES6/exercicio-foguetinho-1.js
+++ fundamentos/secao-3-introducao-a-JS/dia-5-ES6/exercicio-foguetinho-1.js
@@ -4,11 +4,27 @@
 
 // Copie o código a seguir e rode-o para verificar sua saída.
 
-function imprimeIdade() {
-    for (let idade = 30; idade <= 40; idade += 1) {
-      console.log('Idade dentro do for:', idade)
-    }
-  }
-  imprimeIdade()
+// function imprimeIdade() {
+//     for (let idade = 30; idade <= 40; idade += 1) {
+//       console.log('Idade dentro do for:', idade)
+//     }
+//   }
+//   imprimeIdade()
+
+//*CONCLUÍDO*
+
+
+// Altere o valor das propriedades do objeto para que respeite as características da variável do tipo const.
+// Copie o código a seguir e rode-o para verificar sua saída.
+
+// Executando esse código, recebe-se um erro `TypeError: Assignment to constant variable.`
+const pessoa = {
+    nome: 'Henri',
+    idade: 20
+}
+    pessoa["nome"] = 'Luna'
+    pessoa["idade"] = '19'// Altere essa estrutura para corrigir o erro.
+console.log('Nome:', pessoa.nome);
+console.log('Idade:', pessoa.idade);
 
 //*CONCLUÍDO*
