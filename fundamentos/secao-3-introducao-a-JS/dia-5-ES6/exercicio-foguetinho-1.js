7b0dfb565e0ee875bc1f5197762d0d1264d09803
Matheus Freitas <matheus2002fernando@gmail.com>
2023-03-03 19:10:14 -0300

    Concluido 1.4

--- fundamentos/secao-3-introducao-a-JS/dia-5-ES6/exercicio-foguetinho-1.js
+++ fundamentos/secao-3-introducao-a-JS/dia-5-ES6/exercicio-foguetinho-1.js
@@ -33,8 +33,26 @@
 // Modifique a variável para que não ocorra Erro.
 // Copie o código a seguir e rode-o para verificar sua saída.
 
-let favoriteFood = 'Lasanha';
-favoriteFood = 'Hambúrguer';
-console.log(favoriteFood);
+// let favoriteFood = 'Lasanha';
+// favoriteFood = 'Hambúrguer';
+// console.log(favoriteFood);
+
+//*CONCLUÍDO*
+
+
+// Modifique as concatenações para template literals.
+
+// Copie o código a seguir.
+
+const name = 'Adriana';
+const lastName = 'Soares';
+console.log('Olá' + ',' + name + ' ' + lastName + '!');
+function soma(a,b) {
+  let resultado = a + b;
+  return resultado;
+}
+let a = 3;
+let b = 5;
+console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
 
 //*CONCLUÍDO*
