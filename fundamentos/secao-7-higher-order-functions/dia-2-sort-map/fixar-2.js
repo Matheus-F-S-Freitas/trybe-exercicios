048cda9de8fbff9c9843b36220e10dffae2f38cc
Matheus Freitas <matheus2002fernando@gmail.com>
2023-04-04 16:00:36 -0300

    Conclui copiando da Trybe, mas não entendi nada

--- fundamentos/secao-7-higher-order-functions/dia-2-sort-map/fixar-2.js
+++ fundamentos/secao-7-higher-order-functions/dia-2-sort-map/fixar-2.js
@@ -1,10 +1,28 @@
-const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
+// const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
+// const prices = [2.99, 3.99, 1.5, 2];
+
+// const prodPrice = (produ, price) => produ.map((element, index) => 
+//     { [element]: price[index] }
+// )
+
+// console.log(prodPrice(products, prices))
+
+
+// Solução da trybe
+
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
 const prices = [2.99, 3.99, 1.5, 2];
 
-const prodPrice = (produ, price) => produ.map((element, index) => 
-    {[element]: price[index]}
-)
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
 
-console.log(prodPrice(products, prices))
-
-
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
+// ]
